4f32c319830705f14ea2ae7f6bbeba52321eca2a
Takashi Etou <tetor.takashi.etou@gmail.com>
2021-12-04 10:27:49 +0900

    test

--- flask_login/utils.py
+++ flask_login/utils.py
@@ -78,11 +78,15 @@ def make_next_param(login_url, current_url):
 
     if (not l_url.scheme or l_url.scheme == c_url.scheme) and \
             (not l_url.netloc or l_url.netloc == c_url.netloc):
-
-        if (request.headers.get("X-Forwarded-Proto") != c_url.scheme):
-            return urlunparse((request.headers.get("X-Forwarded-Proto"), c_url.netloc, c_url.path, c_url.params, c_url.query, ''))
         return urlunparse(('', '', c_url.path, c_url.params, c_url.query, ''))
-    return current_url
+
+    if (not l_url.netloc or l_url.netloc == c_url.netloc) and \
+            request.headers.get("X-Forwarded-Proto") != c_url.scheme:
+        return urlunparse((request.headers.get("X-Forwarded-Proto"), c_url.netloc, c_url.path, c_url.params, c_url.query, ''))
+
+    # return current_url
+    # TODO: Test
+    return urlunparse(("https", c_url.netloc, c_url.path, c_url.params, c_url.query, ''))
 
 
 def expand_login_view(login_view):
